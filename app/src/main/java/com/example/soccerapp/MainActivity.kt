e05517ccd98b2b8d60034dc42efff9f7e8bf4e9f
Dareus <dareusm@outlook.com>
2023-05-29 18:25:51 -0400

    Editing layout file titles

--- app/src/main/java/com/example/soccerapp/MainActivity.kt
+++ app/src/main/java/com/example/soccerapp/MainActivity.kt
@@ -26,7 +26,6 @@ class MainActivity : AppCompatActivity() {
     lateinit var title: TextView
     lateinit var firstName: String
     lateinit var profileIcon: ImageView
-    lateinit var backArrow: ImageView
     var db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,11 +44,6 @@ class MainActivity : AppCompatActivity() {
         findFieldBtn = findViewById(R.id.find_field_btn)
         reqSessionBtn = findViewById(R.id.request_session_btn)
         profileIcon = findViewById(R.id.profile_Icon)
-        backArrow = findViewById(R.id.back_arrow)
-
-        backArrow.setOnClickListener {
-            finish()
-        }
 
         profileIcon.setOnClickListener {
             val intent = Intent(this, Profile::class.java)
